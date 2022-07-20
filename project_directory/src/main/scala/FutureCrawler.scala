3b9b63c246e7a5ea20aea308930b130fd776558c
BlackTea13 <53616626+BlackTea13@users.noreply.github.com>
2022-07-20 15:09:27 +0700

    futures crawler implemented
    oops forgot to commit for a while

--- project_directory/src/main/scala/FutureCrawler.scala
+++ project_directory/src/main/scala/FutureCrawler.scala
@@ -1,6 +1,5 @@
 import org.jsoup.nodes.Document
 import org.jsoup.{HttpStatusException, Jsoup}
-
 import java.net.{MalformedURLException, URI, URL}
 import java.util.concurrent.atomic.AtomicInteger
 import scala.annotation.tailrec
@@ -18,5 +17,70 @@ object FutureCrawler {
   private val extMap: mutable.Map[String, Int] = TrieMap[String, Int]()
   private val urlMapNonHtml: mutable.Map[String, Int] = TrieMap[String, Int]()
   private var basePath: String = ""
-  
+
+
+  def crawlForStats(basePath: String): WebStats = {
+    this.basePath = basePath
+
+    val mapOfLinks = MiscMethods.bfs(extractLinksFromCurrentPage, this.basePath)
+    createWebStats(mapOfLinks)
+  }
+
+  /**
+   * Define nbrs() function for breadth first search (in MiscMethods)
+   */
+  private def extractLinksFromCurrentPage(url: String): Set[String] = {
+    try {
+      val doc = Jsoup.connect(url).get
+      val allLinks = doc.select("a[href]").asScala.map(link => {
+        try {
+          val uri = URL(link.attr("abs:href")).toURI
+          if uri.getFragment != null
+            then
+              URI(uri.getScheme, uri.getSchemeSpecificPart, null).toURL.toString
+            else
+              link.attr("abs:href")
+        }
+        catch {
+          case _ : Exception => ""
+        }
+      }).filter(_.startsWith(basePath))
+
+      numWords.addAndGet(doc.text().split("\\s+").count(MiscMethods.isWord))
+      addExtensionToMap(doc,"img[src]", "abs:src")
+      addExtensionToMap(doc, "link[href]", "abs:href")
+      addExtensionToMap(doc, "script[src]", "abs:src")
+      addExtensionToMap(doc, "iframe[src]", "abs:src")
+
+      allLinks.filter(_.endsWith(".html")).toSet
+    } catch {
+      case _: Exception => Set()
+    }
+  }
+
+  /**
+   * update map with links in the document with a certain attribute and tag
+   */
+  private def addExtensionToMap(doc: Document, cssQuery: String, attributeKey: String): Unit = {
+    val links = doc.select(cssQuery).asScala.map(_.attr(attributeKey)).filter(link => link.startsWith(basePath))
+    val filter = links.filter(!urlMapNonHtml.contains(_)).map(g => {
+      urlMapNonHtml.put(g, 1)
+      g.toLowerCase().substring(g.lastIndexOf(".")+1)
+    })
+    filter.foreach(l => {
+      if extMap.contains(l) then
+        extMap.update(l, extMap(l) + 1)
+      else
+        extMap.put(l, 1)
+    })
+  }
+
+  /**
+   * create WebStats object from the map of links
+   */
+  private def createWebStats(links: Set[String]) : WebStats = {
+    val mapExtensions_ = extMap.toMap + ("html" -> links.size)
+    WebStats(links.size, extMap.keySet.size, mapExtensions_, numWords.get())
+  }
+
 }
